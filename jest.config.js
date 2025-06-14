f0d01a6cd3730e7325227d27ef2306f2f48c4256
Shin <37034832+shin4488@users.noreply.github.com>
2025-06-14 14:55:59 +0900

    fix jest config for image alias

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
+    '^@/images/(.*)$': '<rootDir>/__mocks__/file-mock.js',
     '^@/(.*)$': '<rootDir>/src/$1',
     '^~/(.*)$': '<rootDir>/$1',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
