a76e48630982e9429ebc96a7fa9f2d41f26d31eb
Shin <37034832+shin4488@users.noreply.github.com>
2025-06-14 14:35:36 +0900

    Add Jest aliases and file mocks

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/file-mock.js',
+  },
+  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 };
